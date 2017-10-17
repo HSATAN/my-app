98623e2594c9bde61f54d8d12dfe24023721976d
edison <2499090390@qq.com>
2017-10-17 21:52:51 +0800

    添加sql

---
+++ spark/sql.py
@@ -0,0 +1,6 @@
+# _*_ coding:utf-8 _*_
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("sql").config("spark_sql","value").getOrCreate()
+df = spark.read.json("/input/age.json")
+df.show()
